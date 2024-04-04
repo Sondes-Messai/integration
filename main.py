5b7cf490f380d8a24a0b6c494331c22cd0408faa
SONDES MESSAI PRO <sondesmessai135@gmail.com>
2024-04-04 11:39:35 +0200

    python

---
+++ main.py
@@ -0,0 +1,5 @@
+fichier = open("main.py", "w")
+fichier.write("<html><body> <h1>  site web generé </h1>")
+fichier.write("<p>ceci est le site du client<b>client1</p>") 
+
+fichier.write("</body></html>")
